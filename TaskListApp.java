8191479ac4e52e01351c2455618d929194800f8d
21331A0584 <kodidasusaimanikanta8341@gmail.com>
2024-05-17 12:06:36 +0530

    Added The user Menu

---
+++ TaskListApp.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+import javax.sound.midi.Soundbank;
+
+public class TaskListApp{
+
+    private ArrayList<String> TaskList = new ArrayList<String>();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        System.out.println("Welcome to Task List Application !!");
+        while(true){
+            System.out.println("1. Add Task");
+            System.out.println("2. Remove/Complete Task");
+            System.out.println("3. View Task");
+            System.out.println("4. Edit Task");
+            System.out.println("5. Exit");
+            System.out.println("Enter Your Choice: ");
+        }
+    }
+}
